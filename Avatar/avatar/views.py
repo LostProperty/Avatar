100af6b2e8441b31ff00fc732a5f8da436a62d0e
Pete Graham <petegraham1@gmail.com>
2014-07-18 10:51:45 +0100

    First version working.

---
+++ Avatar/avatar/views.py
@@ -0,0 +1,27 @@
+import requests
+
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+
+
+def get_youtube_avatar_url(username):
+    try:
+        url = 'https://gdata.youtube.com/feeds/api/users/{0}?alt=json&fields=media:thumbnail'.format(username)
+        response = requests.get(url)
+        data = response.json()
+        return data['entry']['media$thumbnail']['url']
+    except:
+        # Default profile picture
+        return 'https://yt3.ggpht.com/-FN8ix3x-cCI/AAAAAAAAAAI/AAAAAAAAAAA/VK4OSAfDv3w/s77-c-k-no/photo.jpg'
+
+
+@view_config(route_name='home')
+def home(request):
+    # TODO: render a simple template here. Explain what the project is about
+    pass
+
+
+@view_config(route_name='redirect_to_avatar')
+def redirect_to_avatar(request):
+    avatar_url = get_youtube_avatar_url(request.matchdict['username'])
+    return HTTPFound(location=avatar_url)
